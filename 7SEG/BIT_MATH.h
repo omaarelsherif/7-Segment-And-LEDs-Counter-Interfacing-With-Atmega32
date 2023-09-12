450e3b59a920083654a1856954c232614f00cfd1
Omar Elsherif <omaarelsherif@gmail.com>
2023-09-12 23:22:27 +0300

    update BIT_MATH.h

--- 7SEG/BIT_MATH.h
+++ 7SEG/BIT_MATH.h
@@ -8,9 +8,9 @@
 #define BIT_MATH_H
 
 // Macros to manipulate over specific bit in a register 
-#define 	SET_BIT(REG, BIT)         ((REG) |= (1 << BIT))         
-#define     CLR_BIT(REG, BIT)         ((REG) &= (~(1 << BIT)))
-#define     GET_BIT(REG, BIT)         ((REG) >> BIT) & 0x01)
-#define     TOG_BIT(REG, BIT)        ((REG) ^= (1 << BIT))
+#define 	SET_BIT(REG, BIT)		((REG) |= (1 << BIT))         
+#define     CLR_BIT(REG, BIT)       ((REG) &= (~(1 << BIT)))
+#define     GET_BIT(REG, BIT)       (((REG) >> BIT) & 0x01)
+#define     TOG_BIT(REG, BIT)       ((REG) ^= (1 << BIT))
 
 #endif
